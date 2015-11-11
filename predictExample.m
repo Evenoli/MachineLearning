b494e91e603f0ef2dfc631ac2e8bd6fb6636660b
Oliver Robinson <evenoli@hotmail.co.uk>
2015-11-11 12:57:08 +0000

    added predictExample.m

---
+++ predictExample.m
@@ -0,0 +1,16 @@
+function [ prediction ] = predictExample( tree, ex )
+%Given tree 'tree' and example 'ex' (1x45) will return a 1 or a 0
+%corresponding to whether the example appears to match the emotion of the
+%given tree.
+
+    if(tree.op == 'null');
+        prediction = tree.class;
+    else
+        if(ex(1, tree.op))
+            prediction = predictExample(tree.kids{2}, ex);
+        else
+            prediction = predictExample(tree.kids{1}, ex);
+        end
+    end
+end
+
