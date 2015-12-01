c33dac42d2c18928a79eb28dcfdaf1a4e53b9d03
Oliver Robinson <evenoli@hotmail.co.uk>
2015-12-01 23:45:25 +0000

    WHAT DOES IT MEAN??

--- testANN.m
+++ testANN.m
@@ -1,6 +1,6 @@
 function [ predictions ] = testANN( net, x2 )
 
-    results = sim(net, x2);
+    results = sim(net, x2); %% WHAT DOES IT MEAN IF WE GET ZEROS HERE!?!!?
     predictions = NNout2labels(results);
     
     %predictions = NNout2labels(net(x2));
