464566ea697c54fc8eeb4c1901299a3511a56898
Sam Budd <sambudd@dyn1202-243.wlan.ic.ac.uk>
2015-12-03 18:20:20 +0000

    added our trained network to repo

--- PerformEstiNoisy.m
+++ PerformEstiNoisy.m
@@ -1,4 +1,4 @@
-function [ confusion_matrix, class_metrics, acr ] = PerformEstiNoisy( opti_params )
+function [ confusion_matrix, class_metrics, acr, acerr_per_fold ] = PerformEstiNoisy( opti_params )
 %Section VII
 
 load('noisydata_students');
@@ -11,13 +11,13 @@ CROSS_VALIDATION_NUM = 10;
 N_EPOCHS = 100;
 
 con_matricies = cell(CROSS_VALIDATION_NUM, 1);
-num_train_examples = size(x2, 2);
-base_fold_size = floor(num_train_examples/CROSS_VALIDATION_NUM);
+num_examples = size(x2, 2);
+base_fold_size = floor(num_examples/CROSS_VALIDATION_NUM);
 
 %Works out optimal fold sizes. Takes remainder after dividing number of
 %examples by fold size, and distributes extra examples evenly into
 %other folds.
-remainder = rem(num_train_examples, base_fold_size);
+remainder = rem(num_examples, base_fold_size);
 fold_sizes(1:10) = base_fold_size;
 i = 1;
 while(remainder > 0)
@@ -26,21 +26,23 @@ while(remainder > 0)
     i = i + 1;
 end
 
+disp(fold_sizes);
+
 fold_start = 1;
 for j = 1:CROSS_VALIDATION_NUM
     fold_end = fold_start + fold_sizes(j);
-    if(fold_end > num_train_examples);
-        fold_end = num_train_examples;
+    if(fold_end > num_examples);
+        fold_end = num_examples;
     end
 
     %train/test examples
     testx = x2(:, fold_start:fold_end);
-    trainx_2 = x2(:, fold_end+1:num_train_examples);
+    trainx_2 = x2(:, fold_end+1:num_examples);
 
     %train/test Labels
     testy = y2(:, fold_start:fold_end);
-    trainy_2 = y2(:, fold_end+1:num_train_examples);
-
+    trainy_2 = y2(:, fold_end+1:num_examples);
+    
     if(j == 1);
         train_valx = trainx_2;
         train_valy = trainy_2;
@@ -68,24 +70,27 @@ for j = 1:CROSS_VALIDATION_NUM
     %Train net with optimal params
     switch params.training_func
         case 'GD'
-            [ net, tr ] = traingdNet([opti_params.layers, opti_params.neurons], ...
-                                    trainx, trainy, opti_params.lRate, N_EPOCHS);
+            [ net, tr ] = traingdNet([params.layers, params.neurons], ...
+                                    trainx, trainy, params.lRate, N_EPOCHS);
         case 'GDA'
-            [ net, tr ] = traingdaNet([opti_params.layers, opti_params.neurons], ...
-                                    trainx, trainy, opti_params.lRate, opti_params.lr_inc,...
-                                    opti_params.lr_dec, N_EPOCHS);
+            [ net, tr ] = traingdaNet([params.layers, params.neurons], ...
+                                    trainx, trainy, params.lRate, params.lr_inc,...
+                                    params.lr_dec, N_EPOCHS);
         case 'GDM'
-            [ net, tr ] = traingdmNet([opti_params.layers, opti_params.neurons], ...
-                                       trainx, trainy, opti_params.lRate, opti_params.momentum, N_EPOCHS);
+            [ net, tr ] = traingdmNet([params.layers, params.neurons], ...
+                                       trainx, trainy, params.lRate, params.momentum, N_EPOCHS);
         case 'RP'
-            [ net, tr ] = trainrpNet([opti_params.layers, opti_params.neurons], ...
-                                trainx, trainy, opti_params.delt_inc, opti_params.delt_dec, N_EPOCHS);
+            [ net, tr ] = trainrpNet([params.layers, params.neurons], ...
+                                trainx, trainy, params.delt_inc, params.delt_dec, N_EPOCHS);
     end
     
     %Test with test fold!
     [ predictions ] = testANN( net, testx );
     labels = NNout2labels(testy);                    
     con_matricies{j} = confusionMatrixNN(predictions, labels);
+    avgMetrics = calculateAvgMetrics(con_matricies{j});
+    disp(con_matricies{j});
+    acerr_per_fold(1, j) = 1 - avgMetrics.AvgClassificationRate;
 	
     fold_start = fold_end+1;
 end
